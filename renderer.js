7b4f96e729fa41fbcd45bbb4723446ea29d092bb
mario_naeudcs <mr7fenix@gmail.com>
2021-07-31 09:40:57 +0200

    Initial commit

---
+++ renderer.js
@@ -0,0 +1,19 @@
+document.getElementById('add').addEventListener('click', function () {
+    window.open('nuovolibro.html', '_blank', 'frame=true')
+    window.hide();
+})
+
+document.getElementById('search').addEventListener('click', function () {
+    window.open('cercalibro.html')
+})
+
+
+window.onload = function (){
+    let fs = require('fs');
+    let biblioteca = fs.readFileSync('libri.json');
+    biblioteca = JSON.parse(biblioteca);
+    let libri = biblioteca['biblioteca'].length;
+
+    document.getElementById('tot').innerText = 'Libri Posseduti' + ' ' + libri;
+    console.log(libri)
+}
